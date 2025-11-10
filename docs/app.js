891718014047009653111905520dac55b84c4e85
takgames <takps4.upd@gmail.com>
2025-11-10 14:59:57 +0900

    Add files via upload

--- docs/app.js
+++ docs/app.js
@@ -330,6 +330,7 @@ function openComparePicker(mode /* 'A' | 'B' */) {
     // ★ 初期フォーカスはダイアログ本体へ（自動で検索欄に当たるのを防ぐ）
     // q.focus({ preventScroll: true });
 
+
     // 念のため sticky を一度リフロー
     forceStickyRelayout();
   }
